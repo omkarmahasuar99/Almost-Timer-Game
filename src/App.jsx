aa42f092c4d2fa8b2a3b601cd50f4d71b8089fee
omkarmahasuar99 <126793930+omkarmahasuar99@users.noreply.github.com>
2024-03-26 03:15:26 +0530

    add:prettier formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import Player from './components/Player.jsx';
-import TimerChallenge from './components/TimerChallenge.jsx';
+import Player from "./components/Player.jsx";
+import TimerChallenge from "./components/TimerChallenge.jsx";
 
 function App() {
   return (
     <>
       <Player />
       <div id="challenges">
-        <TimerChallenge title="easy" targetTime="1"/>
-        <TimerChallenge title="not so easy" targetTime="5"/>
-        <TimerChallenge title="tough" targetTime="10"/>
-        <TimerChallenge title="pros only" targetTime="15"/>
+        <TimerChallenge title="easy" targetTime="1" />
+        <TimerChallenge title="not so easy" targetTime="5" />
+        <TimerChallenge title="tough" targetTime="10" />
+        <TimerChallenge title="pros only" targetTime="15" />
       </div>
     </>
   );
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,52 +1,55 @@
 import { useRef, useState } from "react";
 import ResultModal from "./ResultModal.jsx";
-export default function TimerChallenge({title,targetTime}) {
-    const timer=useRef();
-    const dialog=useRef();
+export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+  const dialog = useRef();
 
-    const [timeRemaining,setTimeRemaining] = useState(targetTime*1000);
-    const timerIsActive=timeRemaining > 0 && timeRemaining < targetTime*1000;
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
 
-    if (timeRemaining<=0) {
-        clearInterval(timer.current);
-        // setTimeRemaining(targetTime*1000);
-        dialog.current.open();
-    }
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    // setTimeRemaining(targetTime*1000);
+    dialog.current.open();
+  }
 
-    function handleReset(){
-        setTimeRemaining(targetTime*1000);
-    }
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
+  }
 
-    function handleStart(){
-        timer.current=setInterval(() => {
-            setTimeRemaining(timeRemaining=>timeRemaining-10);
-        },10);
-    }
+  function handleStart() {
+    timer.current = setInterval(() => {
+      setTimeRemaining((timeRemaining) => timeRemaining - 10);
+    }, 10);
+  }
 
+  function handleStop() {
+    clearInterval(timer.current);
+    dialog.current.open();
+  }
 
-    function handleStop(){
-        clearInterval(timer.current)
-        dialog.current.open();
-    }
-
-    return(
-        <>
-        <ResultModal ref={dialog} targetTime={targetTime} onReset={handleReset} remainingTime={timeRemaining}/>
-        <section className="challenge">
-            <h2>{title}</h2>
-            <p className="challenge-time">
-                {targetTime} second{targetTime>1?"s":""}
-            </p>
-            <p>
-                <button onClick={timerIsActive?handleStop:handleStart}>
-                    {timerIsActive?"Stop Timer":"Start Challenge"}
-                </button>
-            </p>
-            <p className={timerIsActive?"active":undefined}>
-                {timerIsActive?"Time is Running":"Timer Inactive" }
-            </p>
-        </section>
-        </>
-        
-    )
+  return (
+    <>
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        onReset={handleReset}
+        remainingTime={timeRemaining}
+      />
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop Timer" : "Start Challenge"}
+          </button>
+        </p>
+        <p className={timerIsActive ? "active" : undefined}>
+          {timerIsActive ? "Time is Running" : "Timer Inactive"}
+        </p>
+      </section>
+    </>
+  );
 }
